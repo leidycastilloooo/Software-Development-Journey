6704b699a433d88ad8f50603293ab626817686bb
Leidy Castillo <leidycastillooo15@gmail.com>
2026-02-03 21:14:51 +0100

    Feat: Erstes Java-Programm (Main.java) hinzugefügt

---
+++ 02-Java-Basics/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Mein erstes Java-Programm.
+ * Ziel: Grundlegende Syntax und Systemausgabe verstehen.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Ausgabe in der Konsole
+        System.out.println("Hallo Welt! Ich lerne Java auf meinem Asus A15.");
+        
+        // Ein einfaches Beispiel für Variablen
+        int stundenGelernt = 1;
+        System.out.println("Heute habe ich schon " + stundenGelernt + " Stunde gelernt.");
+    }
+}
