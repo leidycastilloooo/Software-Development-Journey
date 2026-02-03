9f7abdd1374f38b2505d76330df2f74c69b38210
Leidy Castillo <leidycastillooo15@gmail.com>
2026-02-03 21:20:08 +0100

    Feat: Erstes Java-Programm (Main.java) hinzugefügt

---
+++ 02-Java-Basics/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Das ist eine Konsolenausgabe
+        System.out.println("Hallo Welt!");
+        
+        int motivation = 100;
+        System.out.println("Meine Motivation ist bei " + motivation + "%");
+    }
+}
